e4838ac113f162094be72fdbb49113346eacf9ed
Illia <illia.ostrovskyi@gmail.com>
2020-01-24 14:49:54 +0200

    FIxed banner link to profile page

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './banner.scss';
+import {NavLink} from "react-router-dom";
 
 const Banner = () => {
     return (
         <div className="baner">
             <p className="baner_text baner_text_big"> Drupal 8 Web and Mobile Apps development team</p>
             <p className="baner_text baner_text_small"> Make your projects easier with AnyforSoft</p>
-            <button className="button default-btn"><a href="/portfolio">Our portfolio</a></button>
+            <button className="button default-btn"><NavLink to="/portfolio" exact activeClassName="active">Our portfolio</NavLink></button>
         </div>
     );
 }
