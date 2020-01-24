d0024f89c3b14b3500ce04d77ed551f157b177e6
Illia <illia.ostrovskyi@gmail.com>
2020-01-24 15:49:43 +0200

    FIxed banner btn

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
         <div className="baner">
             <p className="baner_text baner_text_big"> Drupal 8 Web and Mobile Apps development team</p>
             <p className="baner_text baner_text_small"> Make your projects easier with AnyforSoft</p>
-            <button className="button default-btn"><NavLink to="/portfolio" exact activeClassName="active">Our portfolio</NavLink></button>
+            <button className="button default-btn banner-btn"><NavLink to="/portfolio" exact activeClassName="active">Our portfolio</NavLink></button>
         </div>
     );
 }
